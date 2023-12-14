2d296d6ceeb81ef2245598ffd577ba26550ff5c7
RedEmperor7 <73673106+RedEmperor7@users.noreply.github.com>
2023-12-14 12:21:42 +0100

    Update presentation.view.php

--- app/View/presentation.view.php
+++ app/View/presentation.view.php
@@ -1,80 +1,91 @@
-<div class="presentation">
+<h2>Notre Groupe</h2>
+        <div class="equipe">
+            <div class="carte">
                 <a href="">
                     <figure>
-                        <img src="Images/XzA0MDAwMTMuanBn.jpg" alt="photo de Aurore">
+                        <img src="https://media.discordapp.net/attachments/1153396669208084602/1180179969909981314/92_REGNIER_Aurore.jpg?ex=657c7b3f&is=656a063f&hm=f1c30113f8d931e10fd9632f9a867bf53853bbd219a8871f2baaf252edf647f5&=&format=webp&width=423&height=563"
+                            alt="photo de Aurore">
                     </figure>
-                            <div class="Informations">
-                                <p class="Nom"> Aurore</p>
-                            </div>
+                    <div class="Informations">
+                        <p class="Nom"> Aurore</p>
+                    </div>
                 </a>
-            </div>
-                <div class="carte">
-                    <a href="">
-                        <figure>
-                             <img src="Images/XzA5Njk2NjEuanBn.jpg" alt="photo de Alban">
-                        </figure>
-                            <div class="Informations">
-                                <p class="Nom"> Alban</p>
-                            </div>
-                    </a>
             </div>
             <div class="carte">
                 <a href="">
                     <figure>
-                         <img src="Images/XzA5Njk2NjEuanBn.jpg" alt="photo de Raphaël">
+                        <img src="https://media.discordapp.net/attachments/1153396669208084602/1180179970857914368/IMG_0145.JPG?ex=657c7b3f&is=656a063f&hm=405721bee469409dc95aad89aa569f981398139fa0d9d35f81e2dc5c18b5f986&=&format=webp&width=423&height=563"
+                            alt="photo de Alban">
                     </figure>
-                        <div class="Informations">
-                            <p class="Nom"> Raphaël</p>
-                        </div>
+                    <div class="Informations">
+                        <p class="Nom"> Alban</p>
+                    </div>
                 </a>
-        </div>
-        <div class="carte">
-            <a href="">
-                <figure>
-                     <img src="Images/XzA5Njk2NjEuanBn.jpg" alt="photo de Joshuoy">
-                </figure>
+            </div>
+            <div class="carte">
+                <a href="">
+                    <figure>
+                        <img src="https://media.discordapp.net/attachments/1153396669208084602/1180179968580403281/57_LECLAIRE_Raphael.jpg?ex=657c7b3f&is=656a063f&hm=b6f0c8706e3c6bcff56708d6c4f8491a2589bc2254570126b694a806f18a2d20&=&format=webp&width=423&height=563"
+                            alt="photo de Raphaël">
+                    </figure>
+                    <div class="Informations">
+                        <p class="Nom"> Raphaël</p>
+                    </div>
+                </a>
+            </div>
+            <div class="carte">
+                <a href="">
+                    <figure>
+                        <img src="https://media.discordapp.net/attachments/1153396669208084602/1180179969075322970/64_MARTIN_Joshuoy.jpg?ex=657c7b3f&is=656a063f&hm=8c3cc352636e39d8368f7b734393171049e43bef8c9cac68a189607c7682f723&=&format=webp&width=423&height=563"
+                            alt="photo de Joshuoy">
+                    </figure>
                     <div class="Informations">
                         <p class="Nom"> Joshuoy</p>
                     </div>
-            </a>
-    </div>
-    <div class="carte">
-        <a href="">
-            <figure>
-                 <img src="Images/XzA5Njk2NjEuanBn.jpg" alt="photo de Emilie">
-            </figure>
-                <div class="Informations">
-                    <p class="Nom"> Emilie</p>
-                </div>
-        </a>
-</div>
-<div class="carte">
-    <a href="">
-        <figure>
-             <img src="Images/XzA5Njk2NjEuanBn.jpg" alt="photo de Emma">
-        </figure>
-            <div class="Informations">
-                <p class="Nom"> Emma</p>
+                </a>
             </div>
-    </a>
-</div>
-<div class="carte">
-    <a href="">
-        <figure>
-             <img src="Images/XzA5Njk2NjEuanBn.jpg" alt="photo de Alicia">
-        </figure>
-            <div class="Informations">
-                <p class="Nom"> Alicia</p>
-            </div>
-    </a>
-</div>
-        <div class="carte">
-            <a href="">
-                <figure>
-                    <img src="Images/XzA5Njk2NjEuanBn.jpg" alt="photo de Nathân">
-                </figure>
+            <div class="carte">
+                <a href="">
+                    <figure>
+                        <img src="https://media.discordapp.net/attachments/1153396669208084602/1180179967414378566/9_BECKER_Emilie.jpg?ex=657c7b3e&is=656a063e&hm=d55cdc4e7a034e843b6dc0b1b8ad4fe4ecd3c7549f624d065375aec6bac97d1a&=&format=webp&width=423&height=563"
+                            alt="photo de Emilie">
+                    </figure>
                     <div class="Informations">
-                    <p class="Nom"> Nathân</p>
-                </div>
-            </a>
+                        <p class="Nom"> Émilie</p>
+                    </div>
+                </a>
+            </div>
+            <div class="carte">
+                <a href="">
+                    <figure>
+                        <img src="https://media.discordapp.net/attachments/1153396669208084602/1180179970383945819/image0.jpg?ex=657c7b3f&is=656a063f&hm=7d7f41304e881723dfe9717afbea097c5a8c4116ccf114ebf3244e686c9995ef&=&format=webp&width=423&height=563"
+                            alt="photo de Emma">
+                    </figure>
+                    <div class="Informations">
+                        <p class="Nom"> Emma</p>
+                    </div>
+                </a>
+            </div>
+            <div class="carte">
+                <a href="">
+                    <figure>
+                        <img src="https://media.discordapp.net/attachments/1153396669208084602/1180179968060313760/43_GOSSIOME_Alicia.jpg?ex=657c7b3f&is=656a063f&hm=615de7e4b1f2faadee0cda007d65d6b8f4194e3e07216f4a5d6c5524e453669c&=&format=webp&width=423&height=563"
+                            alt="photo de Alicia">
+                    </figure>
+                    <div class="Informations">
+                        <p class="Nom"> Alicia</p>
+                    </div>
+                </a>
+            </div>
+            <div class="carte">
+                <a href="">
+                    <figure>
+                        <img src="https://media.discordapp.net/attachments/1153396669208084602/1180179969486377070/74_MOUDIO_BOUENYE_Nathan.jpg?ex=657c7b3f&is=656a063f&hm=c3013929837e8bfe4035eea9ca59047e6220a9769624655273c616b50e8d0519&=&format=webp&width=423&height=563"
+                            alt="photo de Nathân">
+                    </figure>
+                    <div class="Informations">
+                        <p class="Nom"> Nathân</p>
+                    </div>
+                </a>
+            </div>
         </div>
